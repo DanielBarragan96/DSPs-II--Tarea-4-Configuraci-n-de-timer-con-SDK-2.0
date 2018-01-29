b9158098bf87d23296312df4fb0d85543eecffa7
DanielBarragan96 <ie702554@iteso.mx>
2018-01-28 18:21:05 -0600

    First commit

---
+++ leds.c
@@ -0,0 +1,23 @@
+
+#include "leds.h"
+
+
+uint8 initiLeds()
+{
+
+	return TRUE;//there was no error
+}
+
+uint8 ToogleLedStatus()
+{
+
+	return TRUE;//there was no error
+}
+
+Status getLedStatus()
+{
+
+	return TRUE;//there was no error
+}
+
+
